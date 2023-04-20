1bb92cea5a7cc500ca1e96390722f842cbf10529
yinjiaoying <131365172+yinjiaoying@users.noreply.github.com>
2023-04-20 20:48:25 +0800

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,8 @@ public class Main {
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
+        //12312312312312312312312
+
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
